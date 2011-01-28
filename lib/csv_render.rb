cf402318da76290d727d5e1706ed750c192fc7ad
Phillip Ridlen <phillip@balcomagency.com>
2011-01-28 15:07:46 -0600

    column separator => \t

--- lib/csv_render.rb
+++ lib/csv_render.rb
@@ -3,7 +3,7 @@ require "fastercsv"
 ActionController::Renderers.add :csv do |object, options|
   object = [object] if object.class != Array
   column_names = object.first.class.column_names
-  csv = FasterCSV.generate(:col_sep => ";", :row_sep => "\r\n") do |c|
+  csv = FasterCSV.generate(:col_sep => "\t", :row_sep => "\r\n") do |c|
     c << column_names
     object.each do |o|
       c << column_names.collect do |cn|
