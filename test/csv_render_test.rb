ec3ef1c0d53f031935a4d5ef6f6ecf36fda0f4d5
Phillip Ridlen <phillip@balcomagency.com>
2011-01-28 13:35:37 -0600

    Initial Commit

---
+++ test/csv_render_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CsvRenderTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, CsvRender
+  end
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '10e55faf49eeacdd14d477a91521301d69a8d669306e5fe25f955d121b8cbcd425ee05d2a1db2058337b40f79230b1e0cd7d35be4db2ca588bc2221d15ad4cb5'
