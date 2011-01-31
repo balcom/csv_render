d39439ce4e2325b915d40527f7283b40ec788130
Phillip Ridlen <phillip@balcomagency.com>
2011-01-31 11:59:25 -0600

    output as ms-excel

--- lib/csv_render.rb
+++ lib/csv_render.rb
@@ -3,7 +3,7 @@ require "fastercsv"
 ActionController::Renderers.add :csv do |object, options|
   object = [object] if object.class != Array
   column_names = object.first.class.column_names
-  csv = FasterCSV.generate(:col_sep => ";", :row_sep => "\r\n") do |c|
+  csv = FasterCSV.generate(:col_sep => "\t", :row_sep => "\r\n") do |c|
     c << column_names
     object.each do |o|
       c << column_names.collect do |cn|
@@ -12,5 +12,5 @@ ActionController::Renderers.add :csv do |object, options|
     end
   end
   send_data(csv, :filename => "#{object.first.class.to_s.tableize}.csv",
-    :type => "text/csv", :disposition => "attachment")
+    :type => "application/vnd.ms-excel", :disposition => "attachment")
 end
