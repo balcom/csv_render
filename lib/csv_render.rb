99d8d3285271f405eb51c685b17fa9c9b1e05098
Phillip Ridlen <phillip@balcomagency.com>
2011-01-31 11:23:33 -0600

    trying to get the right format for excel

--- lib/csv_render.rb
+++ lib/csv_render.rb
@@ -3,11 +3,11 @@ require "fastercsv"
 ActionController::Renderers.add :csv do |object, options|
   object = [object] if object.class != Array
   column_names = object.first.class.column_names
-  csv = FasterCSV.generate(:col_sep => "\t", :row_sep => "\n") do |c|
+  csv = FasterCSV.generate(:col_sep => ";", :row_sep => "\r\n") do |c|
     c << column_names
     object.each do |o|
       c << column_names.collect do |cn|
-        o.attributes[cn]
+        o.attributes[cn].gsub(/(\r\n|\t)/, ',')
       end
     end
   end
