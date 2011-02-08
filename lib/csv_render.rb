4d249064962b71362d67b00f0a86a0f5fdc0d1e0
Phillip Ridlen <phillip@balcomagency.com>
2011-02-08 11:20:09 -0600

    Choose XLS or CSV

--- lib/csv_render.rb
+++ lib/csv_render.rb
@@ -1,16 +1,27 @@
 require "action_controller"
 require "fastercsv"
-ActionController::Renderers.add :csv do |object, options|
-  object = [object] if object.class != Array
-  column_names = object.first.class.column_names
-  csv = FasterCSV.generate(:col_sep => "\t", :row_sep => "\r\n") do |c|
-    c << column_names
-    object.each do |o|
-      c << column_names.collect do |cn|
-        o.attributes[cn].to_s.gsub(/(\r\n|\t)/, ',')
+
+class CsvRender
+  def self.generate_csv(object, csv_options, file_options)
+    object = [object] if object.class != Array
+    column_names = object.first.class.column_names
+    csv = FasterCSV.generate(csv_options) do |c|
+      c << column_names
+      object.each do |o|
+        c << column_names.collect do |cn|
+          o.attributes[cn].to_s.gsub(/(\r\n|\t)/, ',')
+        end
       end
     end
+    send_data(csv, :filename => "#{object.first.class.to_s.tableize}.#{file_options[:extension]}",
+      :type => file_options[:type], :disposition => "attachment")
   end
-  send_data(csv, :filename => "#{object.first.class.to_s.tableize}.csv",
-    :type => "application/vnd.ms-excel", :disposition => "attachment")
+end
+
+ActionController::Renderers.add :csv do |object, options|
+  CsvRender.generate_csv(object, {:col_sep => "\t", :row_sep => "\r\n"}, {:extension => "csv", :type => "text/csv"})
+end
+
+ActionController::Renderers.add :xls do |object, options|
+  CsvRender.generate_csv(object, {:col_sep => "\t", :row_sep => "\r\n"}, {:extension => "xls", :type => "application/vnd.ms-excel"})
 end
